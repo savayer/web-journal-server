2b6f0d57125522230c0c69cba3fd053067aeaaf1
savayer <sava0094@gmail.com>
2019-06-05 21:23:21 +0800

    delete old removing department category from routes

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -10,7 +10,7 @@ Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
     'namespace'  => 'App\Http\Controllers\Admin',
-], function () { // custom admin routes
-    CRUD::resource('category', 'CategoryCrudController');
+], function () { // custom admin routes    
     CRUD::resource('article', 'ArticleCrudController');
+    CRUD::resource('tag', 'TagCrudController');
 }); // this should be the absolute last line of this file
